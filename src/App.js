63a3ac2d4d2bf30486701aa2cfebda6ac3572264
Faibysh <faybysh.b@gmail.com>
2023-10-25 21:56:53 +0200

    Created gh-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import axios from "axios";
 
-function App() {
+const Tabs = ({ tabs }) => (
+  <ul>
+    {tabs.map((tab) => (
+      <li key={tab.id}>
+        <Link to={`/${tab.id}`}>{tab.title}</Link>
+      </li>
+    ))}
+  </ul>
+);
+
+const App = () => {
+  const [tabs, setTabs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get("/tabs.json").then((response) => {
+      setTabs(response.data);
+      setLoading(false);
+    });
+  }, []);
+
+  const defaultTabId = window.location.pathname.replace("/", "");
+  const initialTab = tabs.find((tab) => tab.id === defaultTabId);
+  const defaultTab = initialTab || tabs[0];
+
+  const sortedTabs = [...tabs].sort((a, b) => a.order - b.order);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1>My first CMS</h1>
+        {loading ? (
+          <p>Loading tabs...</p>
+        ) : (
+          <>
+            <Tabs tabs={sortedTabs} />
+            <Routes>
+              {sortedTabs.map((tab) => (
+                <Route
+                  key={tab.id}
+                  path={`/${tab.id}`}
+                  element={<ComponentLoader path={tab.path} />}
+                />
+              ))}
+              <Route path="/" element={<Navigate to={`/${defaultTab.id}`} />} />
+            </Routes>
+          </>
+        )}
+      </div>
+    </Router>
   );
-}
+};
+
+const ComponentLoader = ({ path }) => {
+  const Component = require(`./${path}`).default;
+
+  return <Component />;
+};
 
 export default App;
---
+++ src/tabs/dummyChart.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DummyChart = () => (
+  <div>
+    <h2>Dummy Chart</h2>
+    <p>This is a sample chart component.</p>
+  </div>
+);
+
+export default DummyChart;
